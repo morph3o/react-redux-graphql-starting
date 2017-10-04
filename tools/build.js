1b808679c86e34d074639408895f37866fb0fa0e
Piero Divasto <pdivasto@nisum.com>
2017-10-04 17:24:41 -0300

    Webpack production configuration

---
+++ tools/build.js
@@ -0,0 +1,30 @@
+/* eslint-disable no-console */
+import webpack from 'webpack';
+import webpackConfig from '../webpack.config.prod';
+import colors from 'colors';
+
+process.env.NODE_ENV = 'production';
+
+console.log('Generating production code...');
+
+webpack(webpackConfig).run((err, stats) => {
+  if(err) {
+    console.log(err.bold.red);
+    return 1;
+  }
+
+  const jsonStats = stats.toJson();
+
+  if (jsonStats.hasErrors) {
+    return jsonStats.errors.map(error => console.log(error.red));
+  }
+
+  if (jsonStats.hasWarnings) {
+    console.log('Webpack generated the following warnings: '.bold.yellow);
+    jsonStats.warnings.map(warning => console.log(warning.yellow));
+  }
+
+  console.log(`Webpack stats: ${stats}`);
+
+  console.log('The app has been compiled in production mode and written t /dist.'); // Build success!
+});
