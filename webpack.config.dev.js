fa360128f0153b8daa76220d2bae39a1dbe107db
Piero Divasto <pdivasto@nisum.com>
2017-10-12 01:15:38 -0300

    Update issue with webpack loaders

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -25,6 +25,7 @@ export default {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
       {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+      {test: /\.(jpe?g|png|gif|ico)$/i, use: [{loader: 'file-loader', options: {name: '[name].[ext]'}}]},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
       {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -22,13 +22,13 @@ export default {
     new webpack.optimize.OccurrenceOrderPlugin(),
     new webpack.DefinePlugin(GLOBALS),
     new ExtractTextPlugin('style.css'),
-    new webpack.optimize.DedupePlugin(), // remove duplicated packages
     new webpack.optimize.UglifyJsPlugin() // minify JS
   ],
   module: {
     loaders: [
       {test: /\.js$/, include: path.join(__dirname, 'src'), loaders: ['babel-loader']},
-      {test: /(\.css)$/, loaders: ['style-loader', 'css-loader']},
+      {test: /(\.css)$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' })},
+      {test: /\.(jpe?g|png|gif|ico)$/i, use: [{loader: 'file-loader', options: {name: '[name].[ext]'}}]},
       {test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file-loader'},
       {test: /\.(woff|woff2)$/, loader: 'url-loader?prefix=font/&limit=5000'},
       {test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url-loader?limit=10000&mimetype=application/octet-stream'},
